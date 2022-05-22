a5e3eb45fefa65159ccb4db151a62a9520bcb3d0
Blaž Solić <blazsolic99@gmail.com>
2022-05-22 14:44:35 +0200

    Add auth layout and basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
+import { RegistrationContainerComponent } from './pages/registration-container/registration-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  	path: '',
+  	component: AuthLayoutComponent,
+    children: [
+      { path: 'register', component: RegistrationContainerComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
+import { RegistrationContainerComponent } from './pages/registration-container/registration-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    RegistrationContainerComponent,
   ],
   imports: [
     BrowserModule,
