f9b6217b1f61af918a8446411f2c379f7d65168d
Blaž Solić <blazsolic99@gmail.com>
2022-05-31 23:48:38 +0200

    Style new post form

--- src/app/pages/user-page/components/post-form/post-form.component.ts
+++ src/app/pages/user-page/components/post-form/post-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-post-form',
@@ -8,6 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PostFormComponent{
   @Output() post: EventEmitter<any> = new EventEmitter();
+  
+  public imageUrl$ = new BehaviorSubject<string>("");
 
   public postForm: FormGroup = this.fb.group({
     content: ['', Validators.required],
@@ -17,6 +20,12 @@ export class PostFormComponent{
 
   public onChange(event: any) {
     if (event.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]); 
+      reader.onload = (event) => { 
+        this.imageUrl$.next(event.target?.result as string) ;
+      }
+
       const file = event.target.files[0];
       this.postForm.patchValue({
         imageSource: file
